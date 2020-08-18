f813872b81d506ac33b3db2a8ade000df1f125e0
popuguy4ik <polina01252@gmail.com>
2020-08-18 20:23:46 +0500

    added module files

---
+++ common/install.sh
@@ -0,0 +1,34 @@
+OTVAL=[ui_print " This module doesn't support ShitUI!" && abort " Aborting..."]
+NAME=$(getprop ro.miui.ui.version.name)
+CODE=$(getprop ro.miui.ui.version.code)
+REGION=$(getprop ro.miui.region)
+MODEL=$(getprop ro.product.system.model)
+CODENAME=$(getprop ro.product.system.device)
+SDK=$(getprop ro.system.build.version.sdk)
+ui_print " Device: $MODEL " && ui_print " Codename: $CODENAME " && ui_print " Android Version SDK: $SDK "
+ui_print " "
+
+if [ $NAME ]; then
+$OTVAL
+fi
+
+if [ $CODE ]; then
+$OTVAL
+fi
+
+if [ $REGION ]; then
+$OTVAL
+fi
+
+if [ ! $API -ge "29" ]; then
+abort "This Module for Android Q and R only!"
+fi
+
+ui_print " "
+ui_print "  And don't forget to sub to "
+ui_print "  my channel in telegram: "
+ui_print "  @oh_no_cr1nge "
+ui_print "  You are giving me motivation "
+ui_print "  to make more modules in future. "
+ui_print " "
+
